e73c7b296886930a48fb870ea84c9f327d58d190
JohiJimenez <86674135+JohiJimenez@users.noreply.github.com>
2022-06-23 18:19:31 -0300

    Desafio 5 Motores de Plantilla

---
+++ Desafio5/mpEjs/src/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const server = app.listen(8080,()=>console.log("Listening on 8080"));
+
+app.set('views','./views')
+app.set('view engine','ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+let products = [];
+
+       // Productos para probar la funcionalidad
+//   [
+//     {
+//       title: 'vestido',
+//       price: '1500',
+//       thumbnail: 'https://cdn3.iconfinder.com/data/icons/fashion-beauty-vol-1/512/dress_women_fashion_apparel-128.png'
+//     },
+//     {
+//       title: 'remera',
+//       price: '900',
+//       thumbnail: 'https://cdn1.iconfinder.com/data/icons/life-style-and-activities-fill-outline/64/shirt-t-tshirt-sport-apparel-clothes-cotton-128.png'
+//     },
+//     {
+//       title: 'pantalon',
+//       price: '3500',
+//       thumbnail: 'https://cdn3.iconfinder.com/data/icons/fashion-beauty-vol-1/512/jeans_denim_trousers_pants-128.png'
+//     }
+//   ]
+
+app.get('/',(req,res)=>{
+
+    res.render('home')
+})
+app.post('/products',(req,res)=>{
+    products.push(req.body);
+    res.redirect('/')
+    console.log(products)
+})
+
+app.get('/products', (req, res) =>{
+
+        res.render('products', {products: products})
+   
+})
