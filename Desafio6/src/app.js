fd742dece5ae79ea639cc2bd96ac3bee83f6df06
JohiJimenez <86674135+JohiJimenez@users.noreply.github.com>
2022-06-30 10:35:25 -0300

    Desafio 6 WebSockets

---
+++ Desafio6/src/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { Server } = require('socket.io');
+const ProductManager = require('./container/ProductManager.js')
+
+PORT = process.env.PORT || 8080;
+const app = express();
+const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+const io = new Server(server);
+
+app.use(express.static(__dirname + '/public'));
+
+const productsService = new ProductManager;
+
+let log = [];
+
+io.on('connection', async socket => {
+    //se repite para que se muestren en tiempo real lo que ya habia antes para cada cliente que se conecte
+    let products = await productsService.getAll();
+    io.emit('productsReg', products)
+
+    socket.on('sendProduct', async data => {
+        await productsService.createProduct(data);
+        let products = await productsService.getAll();
+        io.emit('productsReg', products)
+    })
+
+    socket.broadcast.emit('newUser')
+    socket.on('message', data => {
+        data.time = new Date().toLocaleTimeString()
+        data.date = new Date().toLocaleDateString()
+        log.push(data);
+        io.emit('log', log);
+    })
+
+    socket.on('registered', data => {
+        socket.emit('log', log);
+    })
+
+})
+
---
+++ Desafio6/src/files/products.js
@@ -0,0 +1,8 @@
+[
+  {
+    "title": "vestido",
+    "price": "300",
+    "thumbnail": "https://cdn3.iconfinder.com/data/icons/fashion-beauty-vol-1/512/dress_women_fashion_apparel-128.png",
+    "id": 1
+  }
+]
