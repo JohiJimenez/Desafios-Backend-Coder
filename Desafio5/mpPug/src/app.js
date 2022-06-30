0d1f2a0e1e269580d8bd9890ab0a96b4ff0fd2c7
JohiJimenez <86674135+JohiJimenez@users.noreply.github.com>
2022-06-30 16:26:04 -0300

    Desafio 5 Corecciones

--- Desafio5/mpPug/src/app.js
+++ Desafio5/mpPug/src/app.js
@@ -17,11 +17,6 @@ let products = [];
 //       thumbnail: 'https://cdn3.iconfinder.com/data/icons/fashion-beauty-vol-1/512/dress_women_fashion_apparel-128.png'
 //     },
 //     {
-//       title: 'remera',
-//       price: '900',
-//       thumbnail: 'https://cdn1.iconfinder.com/data/icons/life-style-and-activities-fill-outline/64/shirt-t-tshirt-sport-apparel-clothes-cotton-128.png'
-//     },
-//     {
 //       title: 'pantalon',
 //       price: '3500',
 //       thumbnail: 'https://cdn3.iconfinder.com/data/icons/fashion-beauty-vol-1/512/jeans_denim_trousers_pants-128.png'
