9c263f5270b13908535021afe65e6c3763f7fdf0
JohiJimenez <86674135+JohiJimenez@users.noreply.github.com>
2022-07-06 17:11:31 -0300

    1era Entrega Proyecto Final

---
+++ ProyectoFinal/src/files/products.js
@@ -0,0 +1,42 @@
+[
+  {
+    "title": "Vestido",
+    "description": "Vestido Turquesa",
+    "code": "1111",
+    "price": "1000",
+    "stock": "10",
+    "thumbnail": "http://localhost:8080/img/1646345054941-vestido.jpg",
+    "id": 1,
+    "timestamp": "2022-03-03T22:04:14.963Z"
+  },
+  {
+    "title": "Camisa",
+    "description": "Camisa Hombre",
+    "code": "3333",
+    "price": "3000",
+    "stock": "8",
+    "thumbnail": "http://localhost:8080/img/1646345204990-camisa.jpg",
+    "id": 3,
+    "timestamp": "2022-03-03T22:06:45.024Z"
+  },
+  {
+    "title": "Falda",
+    "description": "Falda Lunares de Colores",
+    "code": "4444",
+    "price": "4000",
+    "stock": "12",
+    "thumbnail": "http://localhost:8080/img/1646346346260-falda.jpg",
+    "id": 4,
+    "timestamp": "2022-03-03T22:25:46.282Z"
+  },
+  {
+    "title": "Falda",
+    "description": "Falda Lunares de Colores",
+    "code": "4444",
+    "price": "2000",
+    "stock": "2",
+    "thumbnail": "http://localhost:8080/img/1646349336980-falda.jpg",
+    "id": 6,
+    "timestamp": "2022-03-03T23:15:37.002Z"
+  }
+]
